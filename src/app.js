47c90cabb725c2f6b40c90fc6d3b31f2b081aa0c
EngineerPehr <dragontoothforge@gmail.com>
2024-03-30 22:41:30 -0600

    Project Paused

---
+++ src/app.js
@@ -0,0 +1,73 @@
+App = {
+    loading: false,
+    contracts: {},
+
+    load: async () => {
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        await App.render()
+    },
+
+    // Found at: https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+    loadWeb3: async () => {
+        if (typeof web3 !== 'undefined') {
+          App.web3Provider = web3.currentProvider
+          web3 = new Web3(web3.currentProvider)
+        } else {
+          window.alert("Please connect to Metamask.")
+        }
+        // Modern dapp browsers...
+        if (window.ethereum) {
+          window.web3 = new Web3(ethereum)
+          try {
+            // Request account access if needed
+            await ethereum.enable()
+            // Acccounts now exposed
+            web3.eth.sendTransaction({/* ... */})
+          } catch (error) {
+            // User denied account access...
+          }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+          App.web3Provider = web3.currentProvider
+          window.web3 = new Web3(web3.currentProvider)
+          // Acccounts always exposed
+          web3.eth.sendTransaction({/* ... */})
+        }
+        // Non-dapp browsers...
+        else {
+          console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    },
+
+    // Get first account from web3
+    loadAccount: async () => {
+        App.account = web3.eth.accounts[0]
+    },
+
+    loadContract: async () => {
+        const recipeBook = await $.getJSON('RecipeBook.json')
+        App.contracts.RecipeBook = TruffleContract(recipeBook)
+        App.contracts.RecipeBook = setProvider(App.web3Provider)
+        App.recipeBook = await App.contracts.RecipeBook.deployed()
+    },
+
+    render: async () => {
+        if (App.loading) {
+            return
+        }
+        App.setLoading(true)
+        // Account
+        $('#account').html(App.account)
+        App.setLoading(false)
+    }
+
+}
+
+$(() => {
+    $(window).load(() => {
+        App.load()
+    })
+})
